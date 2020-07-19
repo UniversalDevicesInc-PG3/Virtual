a4816c9d473ab4ea4859ab01279e1aeb8739a133
markv58 <markvittes@gmail.com>
2020-07-19 07:21:53 -0500

    Update Virtual.py

--- Virtual.py
+++ Virtual.py
@@ -4,7 +4,7 @@
 """
 This is a NodeServer created for Polyglot v2 from a template by Einstein.42 (James Miline)
 This NodeServer was created by markv58 (Mark Vittes) markv58git@gmail.com
-v1.0.9
+v1.0.10
 """
 
 import polyinterface
@@ -437,7 +437,7 @@ class VirtualTempC(polyinterface.Node):
         self.tempVal = _temp
         self.convertTempFromRaw()
         self.convertFtoC()
-        self.retrieveValues()
+
             
         if self.action1 == 1:
             _type = TYPELIST[(self.action1type - 1)]
@@ -523,7 +523,7 @@ class VirtualTempC(polyinterface.Node):
         self.tempVal = command
         self.convertTempFromRaw()
         self.convertFtoC()
-        #self.retrieveValues()
+ 
 
         if self.action1 == 1:
             _type = TYPELIST[(self.action1type - 1)]
