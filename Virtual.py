aad1c5a598953c52d78890fc25c55ef97360bf77
markv58 <markvittes@gmail.com>
2020-07-19 19:49:44 -0500

    Update Virtual.py

--- Virtual.py
+++ Virtual.py
@@ -78,13 +78,13 @@ class Controller(polyinterface.Controller):
         for key,val in self.polyConfig['customParams'].items():
             a = key
             if a == "isy":
-                LOGGER.debug('ISY ip address is %s ', val)
+                #LOGGER.debug('ISY ip address is %s ', val)
                 self.isy = str(val)
             elif a == "user":
-                LOGGER.debug('ISY user is %s', val)
+                #LOGGER.debug('ISY user is %s', val)
                 self.user = str(val)
             elif a == "password":
-                LOGGER.debug('ISY password is %s', val)
+                #LOGGER.debug('ISY password is %s', val)
                 self.password = str(val)
             elif a.isdigit(): 
                 if val == 'switch':
@@ -296,12 +296,15 @@ class VirtualTemp(polyinterface.Node):
 
         self.action1 = existing['action1']# none, push, pull
         self.setDriver('GV6', self.action1)
+        self.setAction1(self.action1)
                        
         self.action1id = existing['action1id'] # 0 - 400
         self.setDriver('GV8', self.action1id) 
-                       
+        self.setAction1id(self.action1id)
+
         self.action1type = existing['action1type'] # State var, State init, Int var, Int init
         self.setDriver('GV7', self.action1type)
+        self.setAction1type(self.action1type)
             
         self.action2 = existing['action2'] 
         self.setDriver('GV9', self.action2)
