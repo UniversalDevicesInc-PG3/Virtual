f8581a12ae1e16bba03aabec3b2c25a86e05004a
markv58 <markvittes@gmail.com>
2020-07-23 22:07:09 -0500

    Update Virtual.py

--- Virtual.py
+++ Virtual.py
@@ -4,7 +4,7 @@
 """
 This is a NodeServer created for Polyglot v2 from a template by Einstein.42 (James Miline)
 This NodeServer was created by markv58 (Mark Vittes) markv58git@gmail.com
-v1.0.14
+v1.0.16
 """
 
 import polyinterface
