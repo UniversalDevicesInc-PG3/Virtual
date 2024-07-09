5c3f0cf8a2ac121877b718893788d057482ad9bd
sejgit <5984155+sejgit@users.noreply.github.com>
2024-07-09 08:36:52 -0400

    switch uom fix

--- nodes/VirtualSwitch.py
+++ nodes/VirtualSwitch.py
@@ -174,7 +174,7 @@ class VirtualSwitch(udi_interface.Node):
     UOM 2 is boolean so the ISY will display 'True/False'
     """
     drivers = [
-        {'driver': 'ST', 'value': 0, 'uom': 2, 'name': "Status"},
+        {'driver': 'ST', 'value': 0, 'uom': 25, 'name': "Status"},
     ]
 
     """
