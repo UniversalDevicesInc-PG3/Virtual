ff54b2cb652a85d27e433884f63119cf63a17e22
sejgit <5984155+sejgit@users.noreply.github.com>
2025-09-29 12:12:38 -0400

    debug

--- utils/node_funcs.py
+++ utils/node_funcs.py
@@ -234,7 +234,7 @@ def pull_from_isy_var(self, var_type: int | str, var_id: int | str, CALC = False
         LOGGER.debug("ISY get response for %s: %s", path, rtxt)
     except PermissionError as exc:
         LOGGER.exception(f"Permission Error on path {path}")
-        self.Notices['permission'] = 'Access to ISY is not Authorized. Go to node configuration and click Allow.'
+        self.controller.Notices['permission'] = 'Access to ISY is not Authorized. Go to node configuration and click Allow.'
         return
     except RuntimeError as exc:
         if 'ISY info not available' in str(exc):
