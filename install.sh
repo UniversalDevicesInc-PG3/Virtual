6a3c799cbc2c0f376736d4ededf6fce3e8ccf057
Stephen Jenkins <sejgit@github.com>
2026-08-30 11:09:35 -0400

    Standardize install.sh for EISY pip/PG3 install reliability.
    Use pip3 with --disable-pip-version-check and verify runtime imports so
    PG3 does not treat pip noise as a failed install. Hunter: drop pip self-upgrade.

--- install.sh
+++ install.sh
@@ -1,2 +1,5 @@
 #!/usr/bin/env bash
-pip install -r requirements.txt --user
+# EISY/FreeBSD: pip may exit non-zero (resolver noise, pip version check)
+# even when packages installed. PG3 treats non-zero install.sh as HTTP 500.
+pip3 install -r requirements.txt --user --disable-pip-version-check
+python3 -c "import udi_interface, yaml, aiohttp"
